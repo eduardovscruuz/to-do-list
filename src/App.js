33eb2f22636371365b01a6516084dfd325ebebc3
eduardovscruuz <eduardovscruuz@gmail.com>
2023-03-17 21:19:15 -0300

    task reader added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
 
 import "./App.css";
 
 const App = () => {
-	return <div className="container"></div>;
+	const [tasks, setTasks] = useState([
+		{
+			id: "1",
+			title: "Estudar",
+			completed: false,
+		},
+		{
+			id: "2",
+			title: "Ler",
+			completed: true,
+		},
+	]);
+
+	return (
+		<>
+			<div className="container">
+				<Tasks tasks={tasks} />
+			</div>
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Task = ({ task }) => {
+	return (
+		<>
+			<h1>{task}</h1>
+		</>
+	);
+};
+
+export default Task;
